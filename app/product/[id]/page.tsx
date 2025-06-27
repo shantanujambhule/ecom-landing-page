3635cf6a753e37a03a07bbeac28c4aa39a5a468d
Shantanu-io <166125364+shantanujambhule@users.noreply.github.com>
2025-06-27 14:18:33 +0530

    product page updated

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,12 +1,14 @@
-'use client';
-
-import React from 'react';
+// app/product/[id]/page.tsx
 import ProductDetail from '@/app/components/ProductDetail';
 import mockProducts from '@/app/data/mockProducts';
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+interface Props {
+  params: { id: string };
+}
+
+export default async function ProductPage({ params }: Props) {
   const productId = parseInt(params.id);
-  const product = mockProducts.find(p => p.id === productId);
+  const product = mockProducts.find((p) => p.id === productId);
 
   if (!product) {
     return (
